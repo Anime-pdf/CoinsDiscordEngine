610b0fd8959b811f91beb5c798e94cbd3c356bb7
Paul <paul.turkovskiy@gmail.com>
2026-01-02 22:52:40 +0200

    clean main class

--- src/main/kotlin/me/animepdf/cde/CoinsDiscordEngine.kt
+++ src/main/kotlin/me/animepdf/cde/CoinsDiscordEngine.kt
@@ -1,7 +1,11 @@
 package me.animepdf.cde
 
+import io.papermc.paper.command.brigadier.Commands
 import io.papermc.paper.plugin.lifecycle.event.types.LifecycleEvents
+import me.animepdf.cde.commands.AddCommand
 import me.animepdf.cde.commands.PayCommand
+import me.animepdf.cde.commands.ReloadCommand
+import me.animepdf.cde.commands.RemoveCommand
 import me.animepdf.cde.config.ConfigContainer
 import org.bukkit.plugin.java.JavaPlugin
 import su.nightexpress.coinsengine.api.CoinsEngineAPI
@@ -9,18 +13,46 @@ import su.nightexpress.coinsengine.api.CoinsEngineAPI
 class CoinsDiscordEngine : JavaPlugin() {
     lateinit var configContainer: ConfigContainer
 
+    fun registerCommands() {
+        lifecycleManager.registerEventHandler(LifecycleEvents.COMMANDS) {
+            val root = Commands.literal("cde")
+            val prefix = Commands.literal(configContainer.generalConfig.prefix)
+
+            val aliases =
+                AddCommand(this).createCommand() +
+                        RemoveCommand(this).createCommand() +
+                        PayCommand(this).createCommand() +
+                        ReloadCommand(this).createCommand()
+
+            for (alias in aliases) {
+                root.then(alias)
+                prefix.then(alias)
+
+                if (configContainer.generalConfig.nonPrefixedCommands) {
+                    it.registrar().register(alias.build())
+                }
+            }
+
+            it.registrar().register(root.build())
+
+            if (configContainer.generalConfig.prefixedCommands) {
+                it.registrar().register(prefix.build())
+            }
+        }
+    }
+
     override fun onEnable() {
         configContainer = ConfigContainer(dataFolder)
         configContainer.loadConfigs()
 
         val currency = CoinsEngineAPI.getCurrency(configContainer.generalConfig.currency)
-        if(currency == null) {
-            logger.severe("Can't find currency, check config!")
+        if (currency == null) {
+            logger.severe("Can't find specified currency, check config!")
+            server.pluginManager.disablePlugin(this)
+            return
         }
 
-        lifecycleManager.registerEventHandler(LifecycleEvents.COMMANDS) {
-            it.registrar().register(PayCommand(this).createCommand().build())
-        }
+        registerCommands()
     }
 
     override fun onDisable() {
